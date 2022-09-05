a431a545f5cf7cfccdd0c60f3a5ebcc079f966a8
Amr Elsekilly <github@amrsekilly.com>
2022-09-05 13:45:22 +0200

    Finished the image filter function

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,7 +30,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get( "/filteredimage", async ( req, res ) => {
+    let { image_url } = req.query;
+    if ( !image_url ) {
+      return res.status(400)
+        .send(`image_url is required`);
+    }
+    let filteredpath = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filteredpath, () => {
+      deleteLocalFiles([filteredpath]);
+    });
+  } );
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
